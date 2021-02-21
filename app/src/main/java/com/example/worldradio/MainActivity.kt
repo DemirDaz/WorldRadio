1f305ad4ef7111b645f0462dfeb07bd1a2d129f6
DemirDaz <46390495+DemirDaz@users.noreply.github.com>
2021-02-21 05:37:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/worldradio/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.worldradio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+        setContentView(R.layout.activity_main)
+    }
+
+}
